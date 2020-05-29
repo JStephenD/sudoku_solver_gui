4464809bd212d32127d7a6757d3d8e3a25aee0fe
jstephend <stephenfishlmao@gmail.com>
2020-05-29 20:46:45 +0800

    algo done

--- sudoku_copy.py
+++ sudoku_copy.py
@@ -13,6 +13,7 @@ H = 407 # added 2 pixels for 3x3 subgrid emphasis with double lines
 win = pygame.display.set_mode((W, H))
 cellw = W // 9
 cellh = H // 9
+TICK_SPEED = 1000 # lower = slower
 
 # COLORS
 WHITE = (255,255,255)
@@ -27,6 +28,7 @@ background.fill(WHITE)
 
 # SAMPLE GAME
 sudoku = '004300209005009001070060043006002087190007400050083000600000105003508690042910300'
+output_file_name = 'sudoku1.png'
 
 # GAME -> original copy, used to backtrack to the previous zero value cell
 game = [list(map(int, list(sudoku[i:i+9]))) for i in range(0, 9*9, 9)]
@@ -89,19 +91,30 @@ def draw_nums(game):
         i+=1
     solution_surface.blit(game_surface, (0,0))
 
-def is_valid(x, y, num):
+def refresh():
+    win.blit(solution_surface, (0, 0))
+    pygame.display.flip()
+    clock.tick(TICK_SPEED)
+
+def is_valid(x, y, num, test=False):
     ''' checks if the number is valid if 
         placed at x, y,
         checks: row, col, subgrid
     '''
     # row
-    if solution[x].count(num) == 1: return False
+    if solution[x].count(num) == 1: 
+        if test: print('row')
+        return False
     #col
-    if [row[y] for row in solution].count(num) == 1: return False
+    if [row[y] for row in solution].count(num) == 1: 
+        if test: print('col')
+        return False
     #grid
     gridx, gridy = x // 3, y // 3
-    grid = [solution[x*gridx+x][y*gridy+y] for x in range(3) for y in range(3)]
-    if grid.count(num) == 1: return False
+    grid = [solution[3*gridx+x][3*gridy+y] for x in range(3) for y in range(3)]
+    if grid.count(num) == 1: 
+        if test: print('grid', grid)
+        return False
     #default
     return True
 
@@ -113,7 +126,7 @@ def backtrack_from(x, y):
     '''
     while True:
         y -= 1
-        if y <= -1:
+        if y == -1:
             y = 8
             x -= 1
             if x == -1:
@@ -146,23 +159,63 @@ def main():
     run = True
     while run:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: run = False
+            if event.type == pygame.QUIT: 
+                print(seen)
+                run = False
 
         draw_bg()
         draw_grids()
-        win.blit(solution_surface, (0,0))
         
         if no_solution: 
             print('no solution found')
             pygame.quit()
+        if x == 9:
+            print('solved')
+            win.blit(solution_surface, (0, 0))
+            pygame.image.save(win, output_file_name)
+            run = False
+            break
+
         if game[x][y] == 0:
-            pass
+            num = None
+            ls = seen[(x, y)]
+            start = ls[-1] if len(ls) else 1
+            for i in range(start, 10):
+                if (x, y) == (0, 4):
+                    print(i, is_valid(x, y, i, True))
+
+                clear_num(x, y)
+                draw_num(i, x, y, col=BLUE, win=solution_surface)
+                refresh()
+
+                if is_valid(x, y, i):
+                    if i in seen[(x, y)]:
+                        continue
+                    num = i
+                    break
+
+            if num is None:
+                seen[(x, y)].clear()
+                clear_num(x, y)
+                refresh()
+                res = backtrack_from(x, y)
+                if isinstance(res, str):
+                    no_solution = True
+                    continue
+                x, y = res
+                solution[x][y] = 0
+                print((x, y))
+                continue
+            else:
+                seen[(x, y)].append(i)
+                solution[x][y] = i
+
+                clear_num(x, y)
+                draw_num(i, x, y, col=BLUE, win=solution_surface)
 
         x, y = next_cell(x, y)
-        print((x,y), game[x][y])
 
-        pygame.display.flip()
-        clock.tick(30)
+        refresh()
     for row1,row2 in zip(game, solution):
         print(row1, row2)
     pygame.quit()
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# GET SCREEN SIZE DIVISIBLE BY 9
-for i in range(400, 500):
-    if i % 9 == 0: print(i)
-
+## GET SCREEN SIZE DIVISIBLE BY 9
+# for i in range(400, 500):
+#     if i % 9 == 0: print(i)
